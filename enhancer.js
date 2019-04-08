b41f352713f9c2f6607bbd782f13376318ac6f19
Aman <amsingh714@gmail.com>
2019-04-08 17:26:55 -0400

    completed success tests

--- enhancer.js
+++ enhancer.js
@@ -5,19 +5,50 @@ module.exports = {
 }
 
 const item = { 
+  originalName: "Longclaw",
   name: "Longclaw",
   type: "weapon",
   durability: 100,
   enhancement: 0
 }
 
-const enhancement = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, "PRI", "DUO", "TRI", "TET", "PEN"
-]
+const enhancement = {
+  16: "PRI", 
+  17: "DUO", 
+  18: "TRI", 
+  19: "TET",
+  20: "PEN"
+}
 
 
 function success(item) {
-  return item;
+  if (!item) { 
+    return null
+  } 
+  if (item.type !== "armor" && item.type !== "weapon") {
+    throw new Error("Type must be armor or weapon!")
+  }
+  if (item.enhancement < 15) {
+    let newNum = item.enhancement + 1;
+    return {
+      originalName: item.originalName,
+      name: `[+${newNum}] ${item.originalName}`,
+      type: item.type,
+      durability: item.durability,
+      enhancement: newNum
+    }
+  }
+
+  if (item.enhancement >= 15) {
+    let newNum = item.enhancement + 1;
+    return {
+      originalName: item.originalName,
+      name: `["${enhancement[newNum]}"] ${item.name}`,
+      type: item.type,
+      durability: item.durability,
+      enhancement: newNum      
+    }
+  }
 }
 
 function fail(item) {
--- enhancer.spec.js
+++ enhancer.spec.js
@@ -1,2 +1,43 @@
 const enhancer = require("./enhancer");
 
+describe("enhancer.js", () => {
+  describe("succeed()", () => {
+    it("should return null if no item is passed", () => {
+      expect(enhancer.success()).toBe(null);
+    });
+
+    it("should enhance the object", () => {
+      const item = {
+        originalName: "Longclaw",
+        name: "[+7] Longclaw",
+        type: "weapon",
+        durability: 80,
+        enhancement: 7
+      };
+
+      const actual = enhancer.success(item);
+
+      const expected = {
+        originalName: "Longclaw",
+        name: "[+8] Longclaw",
+        type: "weapon",
+        durability: 80,
+        enhancement: 8
+      };
+
+      expect(actual).toEqual(expected);
+    });
+
+    it("should return the correct enhancement", () => {
+      const item1 = {
+        name: "Rave Sword",
+        type: "weapon",
+        durability: 80,
+        enhancement: 19
+      };
+
+      expect(enhancer.success(item1).enhancement).toBe(20);
+      expect(enhancer.success(item1).name).toBe('["PEN"] Rave Sword')
+    });
+  });
+});
